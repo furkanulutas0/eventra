b4663e072d716c53c70249914d695409155d795f
Furkan Ulutas <furkanulutas@icloud.com>
2024-12-03 23:32:39 +0300

    init commit

---
+++ api/controllers/auth/controller.ts
@@ -0,0 +1,42 @@
+﻿import { NextFunction, Request, Response } from "express";
+import { supabase } from "./../../utils/supabase";
+class AuthController {
+  signUp = async (req: Request, res: Response, next: NextFunction) => {
+    const { username, email, password, vPassword } = req.body;
+
+    const userRequest = {
+      username,
+      email,
+      password,
+      vPassword,
+    };
+
+    try {
+      const { data, error } = await supabase.from("users").select("username").eq("username", userRequest.username);
+      if (error) throw error;
+      if (data.length > 0) {
+        throw new Error("Bu username zaten kayıtlı!");
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  signIn = async (req: Request, res: Response, next: NextFunction) => {
+    res.json({ data: "Ok" });
+  };
+
+  signOut = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.clearCookie("access_token").json({
+        success: true,
+        message: "User signed out",
+      });
+    } catch (error) {
+      //TODO Error handle edilecek
+      throw new Error("Errors");
+    }
+  };
+}
+
+export default AuthController;
---
+++ api/utils/supabase.ts
@@ -0,0 +1,9 @@
+﻿import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const supabase = createClient(
+  `${process.env.SUPABASE_PROJECT_URL}`,
+  `${process.env.SUPABASE_PUBLIC_ANON_KEY}`
+);
